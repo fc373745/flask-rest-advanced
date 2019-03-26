600ed3297958fe95458bf4a23ae7fff40a39d295
francis <fc373745@gmail.com>
2019-03-26 14:21:24 -0700

    finalized error messages

--- resources/user.py
+++ resources/user.py
@@ -1,88 +1,68 @@
 from flask_restful import Resource, reqparse
-from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-    jwt_refresh_token_required,
-    get_jwt_identity,
-    jwt_required,
-    get_raw_jwt,
-)
-from models.user import UserModel
-from blacklist import BLACKLIST
+from flask_jwt_extended import jwt_required, fresh_jwt_required
+from models.item import ItemModel
 
-_user_parser = reqparse.RequestParser()
-_user_parser.add_argument(
-    "username", type=str, required=True, help="This field cannot be blank."
-)
-_user_parser.add_argument(
-    "password", type=str, required=True, help="This field cannot be blank."
-)
+BLANK_ERROR = "'{}' cannot be blank."
+NAME_ALREADY_EXISTS = "An item with name '{}' already exists."
+ERROR_INSERTING = "An error occurred while inserting the item."
+ITEM_NOT_FOUND = "Item not found."
+ITEM_DELETED = "Item deleted."
 
 
-class UserRegister(Resource):
-    def post(self):
-        data = _user_parser.parse_args()
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "price", type=float, required=True, help=BLANK_ERROR.format("price")
+    )
+    parser.add_argument(
+        "store_id", type=int, required=True, help=BLANK_ERROR.format("store_id")
+    )
 
-        if UserModel.find_by_username(data["username"]):
-            return {"message": "A user with that username already exists."}, 400
+    def get(self, name: str):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json(), 200
+        return {"message": ITEM_NOT_FOUND}, 404
 
-        user = UserModel(**data)
-        user.save_to_db()
+    @fresh_jwt_required
+    def post(self, name: str):
+        if ItemModel.find_by_name(name):
+            return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
 
-        return {"message": "User created successfully."}, 201
+        data = Item.parser.parse_args()
 
+        item = ItemModel(name, **data)
 
-class User(Resource):
-    """
-    This resource can be useful when testing our Flask app. We may not want to expose it to public users, but for the
-    sake of demonstration in this course, it can be useful when we are manipulating data regarding the users.
-    """
+        try:
+            item.save_to_db()
+        except:
+            return {"message": ERROR_INSERTING}, 500
 
-    @classmethod
-    def get(cls, user_id: int):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            return {"message": "User not found."}, 404
-        return user.json(), 200
+        return item.json(), 201
 
-    @classmethod
-    def delete(cls, user_id: int):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            return {"message": "User not found."}, 404
-        user.delete_from_db()
-        return {"message": "User deleted."}, 200
-
-
-class UserLogin(Resource):
-    def post(self):
-        data = _user_parser.parse_args()
-
-        user = UserModel.find_by_username(data["username"])
-
-        # this is what the `authenticate()` function did in security.py
-        if user and safe_str_cmp(user.password, data["password"]):
-            # identity= is what the identity() function did in security.py—now stored in the JWT
-            access_token = create_access_token(identity=user.id, fresh=True)
-            refresh_token = create_refresh_token(user.id)
-            return {"access_token": access_token, "refresh_token": refresh_token}, 200
-
-        return {"message": "Invalid credentials!"}, 401
-
-
-class UserLogout(Resource):
     @jwt_required
-    def post(self):
-        jti = get_raw_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
-        user_id = get_jwt_identity()
-        BLACKLIST.add(jti)
-        return {"message": "User <id={}> successfully logged out.".format(user_id)}, 200
+    def delete(self, name: str):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {"message": ITEM_DELETED}, 200
+        return {"message": ITEM_NOT_FOUND}, 404
+
+    def put(self, name: str):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item:
+            item.price = data["price"]
+        else:
+            item = ItemModel(name, **data)
+
+        item.save_to_db()
+
+        return item.json(), 200
 
 
-class TokenRefresh(Resource):
-    @jwt_refresh_token_required
-    def post(self):
-        current_user = get_jwt_identity()
-        new_token = create_access_token(identity=current_user, fresh=False)
-        return {"access_token": new_token}, 200
+class ItemList(Resource):
+    def get(self):
+        return {"items": [item.json() for item in ItemModel.find_all()]}, 200
